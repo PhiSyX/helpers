def491024de90afe91d1c6623f80441718f310be
PhiSyX <738010+PhiSyX@users.noreply.github.com>
2020-12-29 20:39:59 +0100

    [shared/lang]: ajout d'une fonction wildcard matching
    fix: import type

--- shared/lang.ts
+++ shared/lang.ts
@@ -1,5 +1,47 @@
-import { WHATEVER } from "./types.d.ts";
+import type { WHATEVER } from "./types.d.ts";
+
+import { escapeRegExp } from "./string.ts";
 
 export const isUndefined = ($$1: WHATEVER): boolean => $$1 === undefined;
 export const isNull = ($$1: WHATEVER): boolean => $$1 === null;
 export const isNil = ($$1: WHATEVER): boolean => $$1 == null;
+
+export function iswmcs(
+  needle: string,
+  haystack: string,
+  exceptChars?: string[],
+  flags?: string,
+): boolean {
+  if (!exceptChars) {
+    exceptChars = [];
+  }
+
+  if (!flags) {
+    flags = "";
+  }
+
+  const buildRegexp = escapeRegExp(haystack, exceptChars)
+    .replace(/\\\?/g, ".")
+    .replace(/\\\*/g, ".*");
+  const regexp = new RegExp(buildRegexp, flags);
+  return regexp.test(needle);
+}
+
+export function iswm(
+  needle: string,
+  haystack: string,
+  exceptChars?: string[],
+  flags?: string,
+): boolean {
+  if (!exceptChars) {
+    exceptChars = [];
+  }
+
+  if (!flags) {
+    flags = "";
+  }
+
+  flags += "i";
+
+  return iswmcs(needle, haystack, exceptChars, flags);
+}
--- shared/lang_test.ts
+++ shared/lang_test.ts
@@ -1,6 +1,8 @@
 import { assertEquals } from "https://deno.land/std@0.82.0/testing/asserts.ts";
 
 import { isNil, isNull, isUndefined } from "./lang.ts";
+import { iswm, iswmcs } from "./lang.ts";
+import { higherOrderFunction } from "./utils.ts";
 
 Deno.test(
   "[shared/lang/isNil]: base",
@@ -28,5 +30,66 @@ Deno.test(
     assertEquals(isUndefined(undefined), true);
     assertEquals(isUndefined("undefined"), false);
     assertEquals(isUndefined(null), false);
+  },
+);
+
+Deno.test(
+  "[shared/lang/iswmcs]: simple comparaison",
+  () => {
+    const simpleComparaison = higherOrderFunction(iswmcs, 2)(
+      "PhiSyX",
+    );
+    assertEquals(simpleComparaison("PhiSyX"), true);
+    assertEquals(simpleComparaison("Jules"), false);
+  },
+);
+
+Deno.test(
+  "[shared/lang/iswmcs]: sensible a la casse",
+  () => {
+    const csComparaison = higherOrderFunction(iswmcs, 2)(
+      "PhiSyX",
+    );
+    assertEquals(csComparaison("PhiSyX"), true);
+    assertEquals(csComparaison("phisyx"), false);
+  },
+);
+
+Deno.test(
+  "[shared/lang/iswm]: insensible a la casse",
+  () => {
+    const icsComparaison = higherOrderFunction(iswm, 2)(
+      "PhiSyX",
+    );
+    assertEquals(icsComparaison("PhiSyX"), true);
+    assertEquals(icsComparaison("phisyx"), true);
+  },
+);
+
+Deno.test(
+  "[shared/lang/iswmcs]: comparaison joker",
+  () => {
+    assertEquals(iswmcs("PhiSyX", "PhiS?X"), true);
+    assertEquals(iswmcs("PhiSyX", "phis?x"), false);
+
+    assertEquals(iswmcs("PhiSyX[absent]", "PhiSyX[*]"), true);
+    assertEquals(iswmcs("PhiSyX[absent]", "PhiSyX[??????]"), true);
+
+    assertEquals(iswmcs("Pseudo[occupee]", "Pseudo[??????]"), false);
+    assertEquals(iswmcs("Pseudo[occupee]", "Pseudo[???*???]"), true);
+  },
+);
+
+Deno.test(
+  "[shared/lang/iswmcs]: comparaison exclusion regex",
+  () => {
+    assertEquals(
+      iswmcs("PhiSyX|24ITmXX", "PhiSyX|????[mf]??"),
+      false,
+    );
+    assertEquals(
+      iswmcs("PhiSyX|24ITmXX", "PhiSyX|????[mf]??", ["[", "]"]),
+      true,
+    );
   },
 );
