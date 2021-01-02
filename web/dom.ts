cc993bc9dbe82b8287a99e71ab305412cc82f30c
PhiSyX <738010+PhiSyX@users.noreply.github.com>
2021-01-02 19:38:13 +0100

    [web]: ajout d'utilitaires pour le web

---
+++ web/dom.ts
@@ -0,0 +1,9 @@
+/**
+ * l'Element qui est l'élément racine du document (par exemple, l'élément <html> pour les documents HTML).
+ */
+export const $html: HTMLElement = <HTMLElement> document.documentElement;
+
+/**
+ * Retourne l'élément <body> du document courant.
+ */
+export const $body: HTMLBodyElement = <HTMLBodyElement> document.body;
--- web/env.ts
+++ web/env.ts
@@ -1,7 +1,8 @@
-// @ts-ignore
 export const { userAgent } = window.navigator;
 
 export const isAndroid: boolean = userAgent.indexOf("android") > 0;
 export const isIOS: boolean = /iphone|ipad|ipod|ios/.test(userAgent);
 
 export const isMobile: boolean = isAndroid || isIOS;
+export const isHttps: boolean =
+  document.location.protocol.indexOf("https") === 0;
